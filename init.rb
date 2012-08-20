b26349cf5d68cf34d4ad23caa4614117d42940a5
Matt Henry <greenideas@gmail.com>
2012-08-20 11:55:48 -0400

    handle one-word titles

--- init.rb
+++ init.rb
@@ -29,13 +29,12 @@ class SSRNImporter
     md = {}
 
     md[:filename] = filename
-    md[:ssrn_id] = filename.match(/\d+/)[0]
+    md[:ssrn_id]  = filename.match(/\d+/)[0]
     md[:ssrn_url] = SSRN_BASE_URL + md[:ssrn_id]
 
     ssrn_page = Nokogiri::HTML(open(md[:ssrn_url]))
 
     md[:title] = ssrn_page.css("#abstractTitle h1").text().strip || "Untitled"
-
     md[:authors] = []
     ssrn_page.css("a[title='View other papers by this author'] h2").each do |a|
       md[:authors] << a.text().strip || ""
@@ -44,7 +43,12 @@ class SSRNImporter
     md[:keywords] = ssrn_page.xpath("//b[contains(., 'Keywords:')]/following-sibling::text()").text().strip || ""
 
     set_metadata md
-    File.rename(filename, md[:title].gsub!(/[^0-9A-Za-z\-]/, '-') + ".pdf")
+
+    new_title = (md[:title].match /[^0-9A-Za-z\-]/).nil? ?
+      md[:title] :
+      md[:title].gsub!(/[^0-9A-Za-z\-]/, '-')
+
+    File.rename(filename, new_title + ".pdf")
   end
 
   def set_metadata(md)
